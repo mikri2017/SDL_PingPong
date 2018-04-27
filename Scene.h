47d2bb9cedb4dc77d9cbfdf0f5e9c539fa75d806
mikri2017 <2007mik007@mail.ru>
2018-04-27 15:26:06 +0700

    Virtual functions of Scene class upgraded to pure virtual.
    Перевел виртуальные функции класса Scene в "чисто виртуальные"

--- Scene.h
+++ Scene.h
@@ -6,9 +6,9 @@
 class Scene
 {
 public:
-    virtual void render(SDL_Renderer *renderer);
-    virtual void process_mouse_motion(Sint32 x, Sint32 y);
-    virtual void process_keyboard_keydown(SDL_Keycode keycode);
+    virtual void render(SDL_Renderer *renderer) = 0;
+    virtual void process_mouse_motion(Sint32 x, Sint32 y) = 0;
+    virtual void process_keyboard_keydown(SDL_Keycode keycode) = 0;
 };
 
 #endif // SCENE_H_INCLUDED
