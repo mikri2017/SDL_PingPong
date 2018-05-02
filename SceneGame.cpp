b5337c104d4a8521bfb3430045268810229b3271
AndreyK <sublihim@ya.ru>
2018-05-03 00:50:36 +0300

    improve return in function process_keyboard_keydown

--- SceneGame.cpp
+++ SceneGame.cpp
@@ -139,7 +139,6 @@ gameReaction SceneGame::process_keyboard_keydown(SDL_Keycode keycode)
             ballmgr->flipVertically();
         if(keycode == SDLK_UP)
             ballmgr->flipHorizontally();
-        return gameReaction::gr_ignore;
-
     }
+    return gameReaction::gr_ignore;
 }
